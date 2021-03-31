e4a99f74a0a9a8d63247e496ef319e279f227129
dd66-comm <80191327+dd66-comm@users.noreply.github.com>
2021-03-31 21:50:18 +0800

    Add files via upload

---
+++ ja.java
@@ -0,0 +1,15 @@
+package address;
+
+public class ja {
+	 public static void main(String[]  args) {
+	 address a1=new address();
+	 a1.setCountry("China");
+     a1.setProvince("湖北");
+     a1.setCity("武汉");
+     a1.setSstreet("阳光大道");
+     a1.setPostcode("435566");
+     a1.displayaddress();
+	
+	 }
+
+}
